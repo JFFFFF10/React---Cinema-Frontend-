3f1a08a4c91ccb450dcad91c330f77d60eda5b63
JFFFFF10 <xie1996510@gmail.com>
2023-04-29 16:10:50 +0800

    Include main page

---
+++ src/BsianHomePage/components/footer/Footer.jsx
@@ -0,0 +1,24 @@
+import React from "react"
+import "./footer.css"
+
+const Footer = () => {
+  return (
+    <>
+      <footer>
+        <div className='container'>
+          <div className='box'>
+            <ul className='flex'>
+              <li>Terms of Use</li>
+              <li>Privacy-Policy</li>
+              <li>Blog</li>
+              <li>FAQ</li>
+            </ul>
+            <p>© 2023 BSIAN ENTERTAINMENT.</p>
+          </div>
+        </div>
+      </footer>
+    </>
+  )
+}
+
+export default Footer
---
+++ src/BsianHomePage/components/header/Header.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import "./header.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch, faUser } from "@fortawesome/free-solid-svg-icons";
+
+const Header = () => {
+	const [Mobile, setMobile] = useState(false);
+	return (
+		<>
+			<header>
+				<div className="container flexSB">
+					<nav className="flexSB">
+						<div className="logo">
+							<img src="./images/logo.png" alt="" />
+						</div>
+						{/*<ul className='flexSB'>*/}
+						<ul
+							className={Mobile ? "navMenu-list" : "flexSB"}
+							onClick={() => setMobile(false)}
+						>
+							<li>
+								<a href="/">Home</a>
+							</li>
+							<li>
+								<a href="/">About</a>
+							</li>
+						</ul>
+						<button className="toggle" onClick={() => setMobile(!Mobile)}>
+							{Mobile ? (
+								<i className="fa fa-times"></i>
+							) : (
+								<i className="fa fa-bars"></i>
+							)}
+						</button>
+					</nav>
+					<div className="account flexSB">
+						<FontAwesomeIcon icon={faSearch} />
+						<FontAwesomeIcon icon={faUser} />
+						<button className="logInButton">Log In</button>
+					</div>
+				</div>
+			</header>
+		</>
+	);
+};
+
+export default Header;
