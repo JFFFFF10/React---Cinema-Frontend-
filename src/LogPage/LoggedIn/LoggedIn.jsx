aef518aef32f55f250db17dbe84f614a392b49a2
JFFFFF10 <xie1996510@gmail.com>
2023-04-30 18:21:03 +0800

    userAdmin page

--- src/LogPage/LoggedIn/LoggedIn.jsx
+++ src/LogPage/LoggedIn/LoggedIn.jsx
@@ -3,14 +3,14 @@ import "./LoggedIn.css";
 import Card from "../Card/Card";
 import axios from "axios";
 import LogoutUser from "../Logout/Logout";
-import { Link } from "react-router-dom";
-import UserAdminPage from "../../UserAdminPage/UserAdminPage";
+import { Link, Navigate } from "react-router-dom";
 
 class LoggedIn extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
 			user: {},
+			redirectToUserAdminPage: false, // Add new state to track if should redirect to UserAdminPage
 		};
 	}
 
@@ -32,6 +32,11 @@ class LoggedIn extends React.Component {
 				// Update the user state with the received data
 				console.log(response.data);
 				this.setState({ user: response.data });
+
+				// Check if user is UserAdmin and redirect to UserAdminPage if they are
+				if (response.data.role === "UserAdmin") {
+					this.setState({ redirectToUserAdminPage: true });
+				}
 			})
 			.catch((error) => {
 				console.log(error);
@@ -39,24 +44,25 @@ class LoggedIn extends React.Component {
 	}
 
 	render() {
-		const { user } = this.state;
+		const { user, redirectToUserAdminPage } = this.state;
+
+		if (redirectToUserAdminPage) {
+			return <Navigate to="/UserAdminPage" />;
+		}
 
 		return (
 			<>
-				{user.role === "UserAdmin" ? (
-					<UserAdminPage />
-				) : (
-					<Card>
-						<h1 className="subtitle">You are now logged in as {user.role}</h1>
-						<Link to="/">
-							<button className="back_button" onClick={this.handleLogoutClick}>
-								Log Out
-							</button>
-						</Link>
-					</Card>
-				)}
+				<Card>
+					<h1 className="subtitle">You are now logged in as {user.role}</h1>
+					<Link to="/">
+						<button className="back_button" onClick={this.handleLogoutClick}>
+							Log Out
+						</button>
+					</Link>
+				</Card>
 			</>
 		);
 	}
 }
+
 export default LoggedIn;
