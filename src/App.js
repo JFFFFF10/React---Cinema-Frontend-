bd25fedce90929e4c5616e4e1477a9a9f660b2cf
NamLeeGG <motherclucker6969@gmail.com>
2023-05-12 22:07:00 +0800

    finished the pages for User Admin

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import HomePage from "./BsianHomePage/HomePage/HomePage";
 import AuthPage from "./LogPage/LoginForm/LoginPage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 //User Admin
 import UserAdminPage from "./UserAdminPage/UserAdminPage";
 import UserAdminViewAccPage from "./UserAdminPage/UserAdminViewAccPage";
@@ -8,11 +9,16 @@ import UserAdminSearchAccPage from "./UserAdminPage/UserAdminSearchAccPage";
 import UserAdminAddAccPage from "./UserAdminPage/UserAdminAddAccPage";
 import UserAdminViewPrfPage from "./UserAdminPage/UserAdminViewPrfPage";
 import UserAdminSearchPrfPage from "./UserAdminPage/UserAdminSearchPrfPage";
+import UserAdminAddPrfPage from "./UserAdminPage/UserAdminAddPrfPage";
+
+//Cinema Manager
+import MovieInfoPage from "./CinemaManagerPage/managerSystem/movieInfo";
+import CinemaRoomPage from "./CinemaManagerPage/managerSystem/cinemaRoom";
+import FNBPage from "./CinemaManagerPage/managerSystem/fnb"
+import MovieSessionInfoPage from "./CinemaManagerPage/managerSystem/movieSession"
 
 //Movies
 import MoviesPage from "./BsianHomePage/MoviesPage/MoviesPage";
-import MovieInfo from "./CinemaManagerPage/managerSystem/movieInfo";
-import CinemaRoom from "./CinemaManagerPage/managerSystem/cinemaRoom";
 import SinglePage from "./BsianHomePage/components/watch/SinglePage";
 import MovieSessionPage from "./BsianHomePage/MovieSessionPage/MovieSessionPage";
 
@@ -29,10 +35,16 @@ function App() {
 				<Route path="/UserAdminPage/ua-search-account" element={<UserAdminSearchAccPage />} />
 				<Route path="/UserAdminPage/ua-search-profile" element={<UserAdminSearchPrfPage />} />
 				<Route path="/UserAdminPage/ua-add-account" element={<UserAdminAddAccPage />} />
+				<Route path="/UserAdminPage/ua-add-profile" element={<UserAdminAddPrfPage />} />
 
 				<Route path="/MoviesPage" element={<MoviesPage />} />
-				<Route path="/MovieInfo" element={<MovieInfo />} />
-				<Route path="/CinemaRoom" element={<CinemaRoom />} />
+				<Route path="/MovieInfoPage" element={<MovieInfoPage />} />
+				<Route path="/CinemaRoomPage" element={<CinemaRoomPage />} />
+				<Route path="/FNBPage" element={<FNBPage />} />
+				<Route path="/MovieSessionInfoPage" element={<MovieSessionInfoPage />} />
+
+
+
 				<Route path="/movie/:id" element={<SinglePage />} />
 
 				<Route
